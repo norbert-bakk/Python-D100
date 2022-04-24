888bc03a0e601f150fc01d17193b359b519f70dc
Bakk Norbert <bakknorbert@outlook.com>
2022-04-24 21:53:54 +0200

    Add files via upload

---
+++ d1/d1-1.py
@@ -0,0 +1,13 @@
+print( len( input("What is your name?")))
+
+name = "Jack"
+print(name)
+
+name="Angela"
+print(name)
+
+print ( )
+name = input("What's your name?")
+length = len(name)
+
+print(length)
