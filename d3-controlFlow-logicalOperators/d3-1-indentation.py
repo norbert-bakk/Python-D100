d66b432ccbbc51feb66b2f430c09186cd76f5280
Bakk Norbert <bakknorbert@outlook.com>
2022-04-25 22:12:04 +0200

    Add files via upload

---
+++ d3-controlFlow-logicalOperators/d3-1-indentation.py
@@ -0,0 +1,11 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What's your height in cm?"))
+
+#indentation
+if height >= 120:
+    print("You can ride!")
+else:
+    print("You can't take a ride!")
+
+#comparison operators --> <> <= >= == !=
+
