4ffc3ec054d502e6f854ced82480b84aa8179250
Bakk Norbert <bakknorbert@outlook.com>
2022-05-11 22:47:01 +0200

    Add files via upload

--- d8-FunctionsWithInputs-Arguments&Parameters-CaesarCipher/d8-2-more-inputs.py
+++ d8-FunctionsWithInputs-Arguments&Parameters-CaesarCipher/d8-2-more-inputs.py
@@ -2,4 +2,4 @@ def greet_with(name, location):
     print(f"Hello {name}")
     print(f"Whats it like in {location}?")
 
-greet_with("Norbert", "Paris")
+greet_with(name="Norbert", location="Paris")
